0140deeb148eb0048ac0badee8fc1fa77cc7d243
Julia.Vidoto <j256773@dac.unicamp.br>
2024-10-14 18:31:17 -0300

    Aquivo plumber para principal, dados usando ra256773. No plumber: ver banco, inserir dado,parametros, grafico.

---
+++ plumber.R
@@ -0,0 +1,69 @@
+library(plumber)
+library(ggplot2)
+dados = read.csv("dados_regressao.csv")
+modelo_salvar = lm(y~x+grupo, data=dados)
+saveRDS(modelo_salvar, file = "modelo_regressao.rds")
+
+#* @apiTitle API de Regressão Linear
+#* @apiDescription Esta API possui funções de Regressão Linear
+
+#* Ver o banco
+#* @get /verBanco
+function() {
+  return(dados)
+}
+
+#* Inserir um novo dado
+#* @param x Variável numérica preditora
+#* @param y Variável numérica resposta
+#* @param grupo Variável categórica
+#* @post /insereDado
+function(x, y, grupo) {
+    nova_linha = data.frame(x, grupo, y, momento_registro = lubridate::now()) #formatação não está sendo a mesma
+    dados_atualizados = rbind(dados, nova_linha) #ver se é <<- ou <- ou =
+    readr::write_csv(dados_atualizados, file = "dados_regressao.csv")
+}
+
+#* Calcular parâmetros da Regressão
+#* @serializer json
+#* @get /parametros
+function() {
+    modelo = lm(y~x+grupo, data=dados)
+    saveRDS(modelo, file = "modelo_regressao.rds")
+    coeficientes = modelo$coefficients
+    sigma = summary(modelo)$sigma
+    valores = list(b0 = unname(coeficientes[1]), b1 = unname(coeficientes[2]),
+                   b2 = unname(coeficientes[3]), b3 = unname(coeficientes[4]),
+                   sigma = sigma)
+    return(data.frame(valores))
+}
+
+#* Gráfico de Regressão
+#* @serializer png
+#* @get /grafico
+function() {
+  grafico = dados %>%
+    ggplot(aes(x=x, y=y, col=grupo))+
+    geom_point()+
+    geom_smooth(method = "lm", se=F)+
+    labs(col = "Grupo")+
+    theme_bw()
+  print(grafico)
+}
+
+#* Predição dos dados
+#* @parser json
+#* @serializer json
+#* @get /predicaoBanco
+function(req) {
+  modelo_atualizado = readRDS("modelo_regressao.rds") #VERIFICAR SE ISSO TA CERTO
+  predict(modelo, req$body)
+}
+
+# Programmatically alter your API
+#* @plumber
+function(pr) {
+    pr %>%
+        # Overwrite the default serializer to return unboxed JSON
+        pr_set_serializer(serializer_unboxed_json())
+}
